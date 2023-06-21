72b2f8455f8860a2063f16f827c4a46cc4b2ec50
Rofaell <rofaelmikhael@gamil.com>
2023-06-21 00:09:09 -0700

    functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,87 @@
-include "main.h"
+#include “main.h”
 
 /**
- * print_times_table - Prints the n times table
- *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
- */
-void print_times_table(int n)
-{
-	int a, b, op;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
-		}
-	}
+*print_times_table-prints times table
+
+*@n : time table to use
+
+* return :void
+
+*/
+
+Void print_times_tables(int n)
+
+{
+
+Int a =0, rep, b;
+
+If (n < 0 || n > 15)
+
+Return;
+
+While (a <= n)
+
+{
+
+For (b = 0; b <= n; b++)
+
+{
+
+Rep = a * b;
+
+If (b == 0)
+
+_putchar (‘0’ + rep);
+
+Eles if (rep < 10)
+
+{
+
+_putchar( ‘ ‘);
+
+_putchar (‘ ‘);
+
+_putchar (‘ 0’ + rep);
+
 }
+
+Eles if (rep < 100)
+
+{
+
+_putchar (‘ ‘);
+
+_putchar (‘ 0 ‘ + rep / 10);
+
+_putchar(‘ 0 ‘ + rep % 10);
+
+}
+
+Eles
+
+{
+
+_putchar( ‘ 0 ‘ + rep / 100);
+
+_putchar (‘ 0 ‘ + (rep – 100) / 10);
+
+_putchar (‘ 0 ‘ + rep % 10);
+
+}
+
+If (b < n)
+
+{
+
+_putchar (‘ , ‘);
+
+_putchar (‘ , ‘);
+
+}
+
+}
+
+_putchar (‘ \n ‘ );
+
+A++;
